cfb33651b3cbcdc7a002a56e37c16f0f5f02e6ea
FandiAbas <fandiabas@00gmail.com>
2024-10-09 16:01:49 +0800

    git init laravel-backend-kedai

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+
+    });
+
+    Route::get('/auth', function () {
+        return view('pages.auth.auth-login', ['type_menu' => 'auth']);
+
+        });
+
+
